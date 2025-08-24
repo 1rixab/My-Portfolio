db9c8fe8fefd56102ad89961c6789780bd4c1743
Risabh Yadav <rashiyadav1505@gmail.com>
2025-08-24 23:51:16 +0530

    test2

--- src/components/Main/hero2.jsx
+++ src/components/Main/hero2.jsx
@@ -2,10 +2,11 @@ const Hero2 = (props) => {
   return (
     <>
       <div
-        className={`hero2-maindiv opacity-[60%] top-[62vh] ${props.className}`}
+        className={`hero2-maindiv opacity-[60%] top-[61vh] ${props.className}`}
       ></div>
     </>
   );
 };
 
 export default Hero2;
+  
--- src/components/Main/mainhero.jsx
+++ src/components/Main/mainhero.jsx
@@ -2,22 +2,26 @@ import Navigation from "../Navbar/nav";
 import heroimage from "../Images/hero.png";
 import Hero2 from "./hero2";
 import Hero3 from "./hero3";
+import Mailimage from "../Images/mail.png";
+import Githubimage from "../Images/Github.png";
+import LinkedInimage from "../Images/LinkedIn.png";
 
 const MainHero = () => {
   return (
     <>
-      <div className="bg-black w-full h-[10vh] flex flex-col items-center">
+      <div className="relative bg-amber-700 w-full min-h-screen flex flex-col flex-grow items-center">
         <div className="w-full">
           <Navigation />
         </div>
-        <div className="relative">
+        <div className="flex-grow flex items-end justify-center">
           <img
-            className="h-[87vh] w-[100vw] object-cover mt-[3vh] block"
+            className="h-[82vh] w-full object-cover mt-[3vh] block"
             src={heroimage}
             alt="heroimage"
           />
-          <Hero2 className="absolute"></Hero2>
-          <Hero3 />
+          
+          <Hero2 className="absolute bottom-0 w-full bg-black/70 [clip-path:polygon(0_30%,100%_0,100%_100%,0_100%)]"></Hero2>
+          {/* <Hero3 /> */}
         </div>
       </div>
     </>
